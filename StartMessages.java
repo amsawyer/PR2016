ced325138f402077c78c5207c5a249b0bd09306a
Ali Sawyer <allisonmarysawyer@gmail.com>
2016-05-21 17:24:17 +0200

    Updated StartMessages

--- StartMessages.java
+++ StartMessages.java
@@ -7,6 +7,8 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class StartMessages implements Runnable {
     private long id;
@@ -35,15 +37,30 @@ public class StartMessages implements Runnable {
                 String message = scan.nextLine();
                 // message divisé par les espaces
                 String[] mess_mots = message.split(" ");
+                String mess_type = mess_mots[0];
 
                 // format incorrect du message
-                if (!(Arrays.asList(mess_possibles).contains(mess_mots[0])) || mess_mots.length < 2) {
-                    System.out.println("Message mal formé - assurez-vous que le message " +
+                if (!(Arrays.asList(mess_possibles).contains(mess_type))) {
+                    System.out.println("Mauvais code de message - assurez-vous que le message " +
                             "est d'un format précisé dans l'énoncé du projet");
                 }
-                // message est bon
+                // code de message est bon
                 else {
                     String mess_id = mess_mots[1]; // message id
+
+                    // message is a "APPL"
+                    if(mess_type.equals("APPL")) {
+                        if(mess_mots.length < 5 || !(mess_mots[2].equals("DIFF####"))) {
+                            System.out.println("Message APPL mal formé - assurez-vous que le message " +
+                                    "est du format précisé dans l'énoncé du projet");
+                        }
+                    }
+                    
+                    // si le message est un "TEST," il faut fixer un time-out
+                    else if(mess_type.equals("TEST")) {
+                        Timer timer = new Timer();  
+                        timer.schedule(new Timeout(), 2000); // 2 secondes
+                    }
 
                     // voir si le message a déjà fait le tour de l'anneau
                     if (!(this.deja_vus).contains(mess_id)) {
@@ -65,5 +82,12 @@ public class StartMessages implements Runnable {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+}
+
+class Timeout extends TimerTask {
+    public void run() {
+        System.out.println("Time's up!");
+        System.exit(0);
     }
 }
